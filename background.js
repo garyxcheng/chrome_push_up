90f68d94b6cf696807ee9ac895e433843c2eb35b
gcheng6 <garycheng@berkeley.edu>
2015-10-10 14:28:35 -0700

    all I have

--- background.js
+++ background.js
@@ -1,9 +1,4 @@
-var overlay = document.createElement('div');
-overlay.className="overlayClass";
-document.body.appendChild(overlay);
 
-
-/*
 function startTimer(duration) {
     var timer = duration, minutes, seconds;
     setInterval(function () {
@@ -15,13 +10,33 @@ function startTimer(duration) {
 
         if (--timer < 0) {
             timer = duration;
-            alert("Go do pushups");
+            createOverlay();
         }
+	/*
+	else if (timer==3){
+	    removeOverlay();
+	    }*/
+
     }, 1000);
 }
 
 window.onload = function () {
-    var time = 5*60;
+    var time = 5;
     startTimer(time);
 };
-*/
+
+function createOverlay(){
+    chrome.tabs.query({active.true,currentWindow:true}, function(tabs){
+	    chrome.tabs.sendMessage(tabs[0].id,{please:"createDiv"}, function(respons){
+		    console.log(respons.farewell);
+		});
+	});
+}
+
+function removeOverlay(){
+    chrome.tabs.query({active.true,currentWindow:true}, function(tabs){
+            chrome.tabs.sendMessage(tabs[0].id,{please:"removeDiv"}, function(respons){
+                    console.log(respons.farewell);
+                });
+        });
+}
--- content.js
+++ content.js
@@ -1,8 +1,50 @@
-var overlay = document.createElement('div');
-overlay.className="overlayClass";
-document.body.appendChild(overlay);
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+	if (request.please == "createDiv"){
+	    createOveral();
+	    sendResponse({confirmation: "Successfully created div"});
+	}
+	else if (request.please=="removeDiv"){
+	    removeOverlay();
+	    sendResponse({confirmation: "Successfully removed div"});
+	}
+	
+    });
+
+function createOverlay(){
+    var overlay = document.createElement('div');
+    overlay.id='div1';
+    overlay.className="overlayClass";
+    document.body.appendChild(overlay);
+}
+
+function removeOverlay(){
+    document.body.removeChild(document.getElementById('div1'));
+}
 
 
+    
+/*
+function startTimer(duration) {
+    var timer = duration, minutes, seconds;
+    setInterval(function () {
+	    minutes = parseInt(timer / 60, 10);
+	    seconds = parseInt(timer % 60, 10);
+
+	    minutes = minutes < 10 ? "0" + minutes : minutes;
+	    seconds = seconds < 10 ? "0" + seconds : seconds;
+
+	    if (--timer < 0) {
+		timer = duration;
+		var overlay = document.createElement('div');
+		overlay.className="overlayClass";
+		document.body.appendChild(overlay);
+
+	    }
+	}, 1000);
+}
+
+
+startTimer(3);
 /*
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
     if (request.please == "createDiv"){
