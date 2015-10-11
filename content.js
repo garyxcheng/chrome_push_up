49f117c179f6b99782b6daf14e8c79fd74e4f187
gcheng6 <garycheng@berkeley.edu>
2015-10-11 02:37:21 -0700

    still gotta problem solve the issue

--- content.js
+++ content.js
@@ -1,32 +1,43 @@
 var darkened = false;
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+    minutes = parseInt(request.please / 60, 10);
+    seconds = parseInt(request.please % 60, 10);
     if (request.please != 0 && !darkened){
         createOverlay();
         darkened = true;
-        minutes = parseInt(request.please / 60, 10);
-        seconds = parseInt(request.please % 60, 10);
         sendResponse({confirmation: "Successfully created div"});
     } else if (request.please == "0" && darkened){
         removeOverlay();
         darkened = false;
         sendResponse({confirmation: "Successfully removed div"});
-    }
+    }/*else if (request.please!=0){
+	big_string=String(minutes)+":"+String(seconds);
+	editOverlay("timer","innerHTML",big_string);
+	}*/
+
 });
 
 function createOverlay(){
+    
     var pElement=document.createElement('h1');
     pElement.id='MainTitle';
-    pElement.innerHTML = "You need to do pushups";
+    pElement.innerHTML = "Go do some pushups";
     pElement.className="pElementClass";
-    
+    /*
+    var p2Element=document.createElement('h1');
+    p2Element.id='timer';
+    p2Element.innerHTML = "00:00";
+    p2Element.className-"p2ElementClass";
+    */
     var iElement=document.createElement('img');
     iElement.src="http://i.imgur.com/gZydMTQ.gif";
     iElement.className="iElementClass";
-    iElement.id='PictureTitle'
+    iElement.id='PictureTitle';
    
     overlay = document.createElement('div');
     overlay.id = 'div';
     overlay.appendChild(pElement);
+    //overlay.appendChile(p2Element);
     overlay.appendChild(iElement);
     overlay.className="overlayClass";
     document.body.appendChild(overlay);
