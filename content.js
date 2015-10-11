d44781b6bd85aa4a29326cf0914fa9954ab1c3f2
gcheng6 <garycheng@berkeley.edu>
2015-10-10 22:40:30 -0700

    fixed the image showing up

--- content.js
+++ content.js
@@ -15,15 +15,15 @@ function createOverlay(){
     pElement.id='MainTitle';
     pElement.innerHTML = "You need to do pushups";
     pElement.className="pElementClass";
-    /*
+    
     var iElement=document.createElement('img');
     iElement.src="http://i.imgur.com/gZydMTQ.gif";
     iElement.className="iElementClass";
-    */
+    
     overlay = document.createElement('div');
     overlay.id = 'div';
     overlay.appendChild(pElement);
-    //overlay.appendChild(iElement);
+    overlay.appendChild(iElement);
     overlay.className="overlayClass";
     document.body.appendChild(overlay);
 }
