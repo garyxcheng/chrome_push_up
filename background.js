6163238cb043de1520240cfbef791a76371b84fb
XivXyv <dennisl88@berkeley.edu>
2015-10-10 21:28:19 -0700

    Options in progress

--- background.js
+++ background.js
@@ -1,4 +1,38 @@
-function startTimer(duration) {
+// Conditionally initialize the options.
+if (!localStorage.isInitialized) {
+  localStorage.isActivated = true;   // The display activation.
+  localStorage.light = 3;        
+  localStorage.dark = 2;
+  localStorage.isInitialized = true; // The option initialization.
+}
+console.log(JSON.parse(localStorage.isActivated));
+console.log(localStorage.light);
+var timer = 0, minutes, seconds;
+setInterval(function () {
+    minutes = parseInt(timer / 60, 10);
+    seconds = parseInt(timer % 60, 10);
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+    seconds = seconds < 10 ? "0" + seconds : seconds;
+    if(JSON.parse(localStorage.isActivated)){
+        duration = localStorage.dark + localStorage.light;
+        dark = localStorage.dark;
+        console.log(duration);
+        console.log(dark);
+        if (--timer < 0) {
+            timer = duration;
+            createOverlay();
+        } else if (timer == dark){
+            removeOverlay();
+        }
+    }
+}, 1000);
+/*
+window.onload = function () {
+    var time = 5;
+    var dark = 2;
+    startTimer(time, dark);
+};
+function startTimer(duration, dark) {
     var timer = duration, minutes, seconds;
     setInterval(function () {
         minutes = parseInt(timer / 60, 10);
@@ -6,24 +40,21 @@ function startTimer(duration) {
 
         minutes = minutes < 10 ? "0" + minutes : minutes;
         seconds = seconds < 10 ? "0" + seconds : seconds;
-
-        if (--timer < 0) {
-            timer = duration;
-            createOverlay();
-        } else if (timer == 3){
-            removeOverlay();
+        if JSON.parse(localStorage.isActivated){
+            if (--timer < 0) {
+                timer = duration;
+                createOverlay();
+            } else if (timer == dark){
+                removeOverlay();
+            }
         }
     }, 1000);
-}
-window.onload = function () {
-    var time = 5;
-    startTimer(time);
-};
+}*/
 
 function createOverlay(){
   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
     chrome.tabs.sendMessage(tabs[0].id, {please: "createDiv"}, function(response) {
-      console.log(response.farewell);
+      
     });
   });
 }
@@ -31,7 +62,7 @@ function createOverlay(){
 function removeOverlay(){
    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
     chrome.tabs.sendMessage(tabs[0].id, {please: "removeDiv"}, function(response) {
-      console.log(response.farewell);
+      
     });
   });
 }
---
+++ options.js
@@ -0,0 +1,23 @@
+/*function ghost(isDeactivated) {
+  options.style.color = isDeactivated ? 'graytext' : 'black';
+                                              // The label color.
+  options.frequency.disabled = isDeactivated; // The control manipulability.
+}*/
+
+window.addEventListener('load', function() {
+  // Initialize the option controls.
+  options.isActivated.checked = JSON.parse(localStorage.isActivated);
+                                         // The display activation.
+  options.dark.value = localStorage.dark;
+  options.light.value = localStorage.light;
+  if (!options.isActivated.checked) { ghost(true); }
+
+  // Set the display activation and frequency.
+  options.isActivated.onchange = function() {
+    localStorage.isActivated = options.isActivated.checked;
+  };
+  options.frequency.onchange = function() {
+    localStorage.dark = options.dark.value;
+    localStorage.light = options.light.value;
+  };
+});
