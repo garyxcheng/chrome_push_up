12a26dd6577168ebdb759cf3719812ae5ebbf5b9
gcheng6 <garycheng@berkeley.edu>
2015-10-11 03:01:41 -0700

    display a timer on the webpage

--- content.js
+++ content.js
@@ -6,14 +6,24 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
         createOverlay();
         darkened = true;
         sendResponse({confirmation: "Successfully created div"});
-    } else if (request.please == "0" && darkened){
+    }else if (request.please == "0" && darkened){
         removeOverlay();
         darkened = false;
         sendResponse({confirmation: "Successfully removed div"});
-    }/*else if (request.please!=0){
-	big_string=String(minutes)+":"+String(seconds);
-	editOverlay("timer","innerHTML",big_string);
-	}*/
+    }if (request.please!=0){
+	if (minutes<10)
+	    minutes="0"+String(minutes);
+	else
+	    minutes=String(minutes);
+	if (seconds<10)
+	    seconds="0"+String(seconds);
+	else
+	    seconds=String(seconds);
+
+	big_string=minutes+":"+seconds;
+	document.getElementById('timer').innerHTML=big_string;
+	//editOverlay("timer","innerHTML",big_string);
+    }
 
 });
 
@@ -23,12 +33,12 @@ function createOverlay(){
     pElement.id='MainTitle';
     pElement.innerHTML = "Go do some pushups";
     pElement.className="pElementClass";
-    /*
+    
     var p2Element=document.createElement('h1');
     p2Element.id='timer';
-    p2Element.innerHTML = "00:00";
-    p2Element.className-"p2ElementClass";
-    */
+    p2Element.innerHTML = "";
+    p2Element.className="pElementClass";
+    
     var iElement=document.createElement('img');
     iElement.src="http://i.imgur.com/gZydMTQ.gif";
     iElement.className="iElementClass";
@@ -37,7 +47,7 @@ function createOverlay(){
     overlay = document.createElement('div');
     overlay.id = 'div';
     overlay.appendChild(pElement);
-    //overlay.appendChile(p2Element);
+    overlay.appendChild(p2Element);
     overlay.appendChild(iElement);
     overlay.className="overlayClass";
     document.body.appendChild(overlay);
@@ -47,6 +57,7 @@ function removeOverlay(){
     document.body.removeChild(document.getElementById('div'));
 }
 
+//VVVVV this doesn't work the way it should
 function editOverlay(objId,attri,new_def){
     document.getElementById(objId).setAttribute(attri,new_def);
 }
